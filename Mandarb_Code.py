206fce39a66352f4472558e15933d3637d221ef0
Mendo Cath <cooper@nancehall.com>
2018-04-27 17:29:06 +0000

    reworked logging

--- Mandarb_Code.py
+++ Mandarb_Code.py
@@ -1,90 +1,75 @@
 import discord
- from discord.ext import commands
- import asyncio
- import logging
- import random
- import datetime
- import configparser
+from discord.ext import commands
+import asyncio
+import logging
+import random
+import datetime
+import configparser
+from time import gmtime, strftime
 
- config = configparser.RawConfigParser()
- configFilePath = r'bot_config.conf'
+config = configparser.RawConfigParser()
+configFilePath = r'bot_config.conf'
 
- try:
-     config.read(configFilePath)
- except:
-     print("Error loading config!  Please check config file 'bot-config.conf'")
+try:
+    config.read(configFilePath)
+except:
+    print("Error loading config!  Please check config file 'bot-config.conf'")
 
- DISCORD_AUTH_TOKEN = config.get('bot-config', 'discord_auth_token')
+DISCORD_AUTH_TOKEN = config.get('bot-config', 'discord_auth_token')
 
- logger = logging.getLogger('discord')
- logger.setLevel(logging.INFO)
- handler = logging.FileHandler(filename='discord.log', encoding='utf-8')
- handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
- logger.addHandler(handler)
+def tchatLogger(server, channel, author, messagetype, message = ""):
+    with open(server + "_" + channel + strftime('_%Y%m%d')+ ".log", 'a') as the_file:
+        the_file.write(strftime('%Y-%m-%d_%H:%M:%S') + "§" + author + "§" + messagetype + "§" + message+"\n")
 
- generallogger = logging.getLogger('discord.general')
- generallogger.setLevel(logging.INFO)
- handler = logging.FileHandler(filename='discord.general.log', encoding='utf-8')
- handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
- generallogger.addHandler(handler)
+description = ''' Test bot for Mendo'''
 
- channel2logger = logging.getLogger('discord.channel2')
- channel2logger.setLevel(logging.INFO)
- handler = logging.FileHandler(filename='discord.channel2.log', encoding='utf-8')
- handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
- channel2logger.addHandler(handler)
+bot = commands.Bot(command_prefix='!', description=description)
 
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
 
- description = ''' Mandarb bot for TV Discord '''
-
- bot = commands.Bot(command_prefix='!', description=description)
- @bot.event
- async def on_ready():
-     print('Logged in as')
-     print(bot.user.name)
-     print(bot.user.id)
-     print('------')
-
- @bot.event
- async def on_message(message):
-     cattachments = ""
-     if len(message.attachments) != 0:
-         cattachments = ''.join(str(e) for e in message.attachments)
-     if message.channel.name == "general":
-         generallogger.info(message.author.name + " : " + message.content + " : " + cattachments)
-     if message.channel.name == "channel2":
-         channel2logger.info(message.author.name + " : " + message.content + " : " + cattachments)
-
-     await bot.process_commands(message)
-
- @bot.event
- async def on_message_delete(message):
-     cattachments = ""
-     if len(message.attachments) != 0:
-         cattachments = ''.join(str(e) for e in message.attachments)
-     if message.channel.name == "general":
-         generallogger.info("Deleted : " + message.author.name + " : " + message.content + " : " + cattachments)
-     if message.channel.name == "channel2":
+@bot.event
+async def on_message(message):
+    cattachments = ""
+    if len(message.attachments) != 0:
         cattachments = ''.join(str(e) for e in message.attachments)
-         channel2logger.info("Deleted : " + message.author.name + " : " + message.content + " : " + cattachments)
+    tchatLogger(str(message.server),str(message.channel),message.author.name,"Text",message.content + (("§" + cattachments) if cattachments != "" else "" ))
+    await bot.process_commands(message)
 
- @bot.event
- async def on_message_edit(before, after):
-     if before.channel.name == "general":
-         generallogger.info("Edit: " + before.author.name + " : " + before.content + " -> " + after.author.name +     " :  " +  after.content)
-     if before.channel.name == "channel2":
-         channel2logger.info("Edit: " + before.author.name + " : " +  before.content + " -> " + after.author.         name +    " : " + after.content)
+@bot.event
+async def on_message_delete(message):
+    cattachments = ""
+    if len(message.attachments) != 0:
+        cattachments = ''.join(str(e) for e in message.attachments)
+    tchatLogger(str(message.server),str(message.channel),message.author.name,"Deleted",message.content +  (("§" + cattachments) if cattachments != "" else "" ))
+@bot.event
+async def on_message_edit(before, after):
+    tchatLogger(str(before.server),str(before.channel),before.author.name,"Edit",before.content + " -> " + after.author.name +    "§" + after.content)
+@bot.event
+async def on_reaction_add(reaction, user):
+    tchatLogger(str(reaction.message.server),str(reaction.message.channel),reaction.message.author.name,"Reaction_add",str(reaction.emoji) + "§" + reaction.message.content )
+@bot.event
+async def on_reaction_remove(reaction, user):
+    tchatLogger(str(reaction.message.server),str(reaction.message.channel),reaction.message.author.name,"Reaction_remove",str(reaction.emoji) + "§" + reaction.message.content )
 
- @bot.command()
- async def roll(dice : str):
-     """Rolls a dice in NdN format."""
-     try:
-         rolls, limit = map(int, dice.split('d'))
-     except Exception:
-         await bot.say('Format has to be in NdN!')
-         return
 
-     result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-     await bot.say(result)
+@bot.command()
+async def roll(dice : str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await bot.say('Format has to be in NdN!')
+        return
 
- bot.run(DISCORD_AUTH_TOKEN)
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await bot.say(result)
+
+bot.run(DISCORD_AUTH_TOKEN)
+
+
+    
